16488bddbbecbfbe9fd7c3d4226c813552e6d56d
Bryan Snyder <snyderb.de@gmail.com>
2023-03-26 14:27:31 -0400

    removed change pomo function, shellchecking

--- f-pomo.sh
+++ f-pomo.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# TODO: Add a progress bar
 # TODO: Ask user to pick a task
 # TODO DB to store tasks... can this connect to notion?
 
 declare -A pomo_options
-pomo_options["work"]="1"
-pomo_options["break"]="2"
+pomo_options["work"]=1
+pomo_options["break"]=1
 
 pomodoro() {
   val=$1
   echo "$val" | lolcat
-  sleep "${pomo_options["$val"]}m"
+  for i in $(seq "${pomo_options["$val"]}" -1 1); do
+    progress=$((100 - i*100/pomo_options["$val"]))
+    echo "$i" | spd-say -w
+    echo -ne "[$(printf "%-${progress}s" "")$(printf "%$((100-progress))s" "" )] $i min \r"
+    sleep 1m
+  done
   spd-say "'$val' session done"
   notify-send --app-name=Pomodoro🍅 "'$val' session done 🍅"
 }
@@ -31,14 +35,5 @@ start_pomodoro() {
   done
 }
 
-change_pomo() {
-  if [ -n "$1" ] && [ -n "$2" ]; then
-    pomo_options["$1"]="$2"
-    echo "The $1 time has been changed to $2 minutes"
-  else
-    echo "Please provide valid parameters: change_pomo [work/break] [time_in_minutes]"
-  fi
-}
-
 # call the function
-pomodoro work
+start_pomodoro
